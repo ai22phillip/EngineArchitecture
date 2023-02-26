81651a9f60d1a87f761b525e6d414794aaf047e0
lorenzo <lorenzothescout@gmail.com>
2023-02-26 19:54:25 +0100

    added premake build project

---
+++ premake5.lua
@@ -0,0 +1,107 @@
+workspace "PowerEngine"
+	architecture "x64"
+	
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+	outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+	
+project "PowerEngine"
+	location "PowerEngine"
+	kind "SharedLib"
+	language "C++"
+	
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+	
+	filter "system:windows"
+		cppdialect "C++20"
+		staticruntime "On"
+		systemversion "latest"
+		
+		defines
+		{
+			"PW_PLATFORM_WINDOWS",
+			"PW_BUILD_DLL"
+		}
+		
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+		
+	filter "configurations:Debug"
+		defines "PW_DEBUG"
+		symbols "On"
+		
+	filter "configurations:Release"
+		defines "PW_RELEASE"
+		optimize "On"
+		
+	filter "configurations:Dist"
+		defines "PW_DIST"
+		optimize "On"
+
+		
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+	
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+	
+	includedirs
+	{
+		"PowerEngine/vendor/spdlog/include",
+		"PowerEngine/src"
+	}
+	
+	links
+	{
+		"PowerEngine"
+	}
+	
+	filter "system:windows"
+		cppdialect "C++20"
+		staticruntime "On"
+		systemversion "latest"
+		
+		defines
+		{
+			"PW_PLATFORM_WINDOWS"
+		}
+		
+		filter "configurations:Debug"
+		defines "PW_DEBUG"
+			symbols "On"
+			
+		filter "configurations:Release"
+		defines "PW_RELEASE"
+			optimize "On"
+		
+		filter "configurations:Dist"
+		defines "PW_DIST"
+			optimize "On"
+			
